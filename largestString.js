ec4b5d9be8824baceed3bc4944dcb413dac470e2
Ayush Tyagi <ak7046243581@gmail.com>
2023-01-16 01:36:08 +0530

    feat: add finding largest string in array

---
+++ largestString.js
@@ -0,0 +1,26 @@
+function largestString(arr) {
+  let len = Number.MIN_SAFE_INTEGER;
+  ans = '';
+  arr.forEach(function (i) {
+    len = Math.max(len, i.length);
+    ans = len == i.length ? i : ans;
+  });
+  return ans;
+}
+
+arr = ['we', 'love', 'code', 'academy'];
+console.log(largestString(arr));
+
+const largestStringA = () => {
+  let len = Number.MIN_SAFE_INTEGER;
+  ans = '';
+  arr.forEach((i) => {
+    len = Math.max(len, i.length);
+    ans = len == i.length ? i : ans;
+  });
+  return ans;
+};
+arr = ['apple', 'peach', 'orange'];
+console.log(largestStringA(arr));
+
+module.exports = { largestString, largestStringA };
